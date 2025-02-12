3dd739d7b8c3bfff574c5e8680e5986b2e41143f
michaeltaufa <michael.taufa@gmail.com>
2025-02-12 14:03:21 -0600

    Update: Learned about looping though dictionaries.

---
+++ user_nvim_test.py
@@ -0,0 +1,11 @@
+names = []
+
+names.append(input("Please enter a name:"))
+names.append(input("Please enter a name:"))
+names.append(input("Please enter a name:"))
+names.append(input("Please enter a name:"))
+names.append(input("Please enter a name:"))
+names.append(input("Please enter a name:"))
+names.append(input("Please enter a name:"))
+
+print(names)
