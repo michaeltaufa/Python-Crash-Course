a25e241b0862f36c609c006e26875cfacb40b11f
michaeltaufa <michael.taufa@gmail.com>
2025-02-15 03:14:43 -0600

    Update: Tested to see input of whitesaces accepted.

--- user_nvim_test.py
+++ user_nvim_test.py
@@ -1,11 +1,13 @@
 names = []
 
-names.append(input("Please enter a name:"))
-names.append(input("Please enter a name:"))
-names.append(input("Please enter a name:"))
-names.append(input("Please enter a name:"))
-names.append(input("Please enter a name:"))
-names.append(input("Please enter a name:"))
-names.append(input("Please enter a name:"))
+# The following block of code below will accept white spaces, which will impact the input.
+
+names.append(input("Please enter a word:"))
+names.append(input("Please enter a first name:"))
+names.append(input("Please enter a location:"))
+names.append(input("Please enter a sports team:"))
+names.append(input("Please enter a food:"))
+names.append(input("Please enter a color:"))
+names.append(input("Please enter a hero name:"))
 
 print(names)
